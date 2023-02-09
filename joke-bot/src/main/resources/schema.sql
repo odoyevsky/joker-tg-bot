f6741166f3356b2ebd7468fedebe8a21f340f1f0
odoyevsky <odoyevsky@aurora>
2023-02-09 23:37:39 +0300

    Model: change h2 schema to postgres

--- joke-bot/src/main/resources/schema.sql
+++ joke-bot/src/main/resources/schema.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS users(
-    user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_id SERIAL PRIMARY KEY,
     user_name VARCHAR(255) NOT NULL,
     chat_id BIGINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS categories(
-    category_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    category_id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS jokes(
-    joke_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    joke_id SERIAL PRIMARY KEY,
     category_id BIGINT NOT NULL,
     text VARCHAR(5000) NOT NULL,
     FOREIGN KEY (category_id) REFERENCES categories(category_id)
@@ -22,3 +22,11 @@ CREATE TABLE IF NOT EXISTS favourite_jokes(
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (joke_id) REFERENCES jokes(joke_id)
 );
+
+CREATE TABLE IF NOT EXISTS rates(
+    user_id BIGINT NOT NULL,
+    joke_id BIGINT NOT NULL,
+    rates BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (joke_id) REFERENCES jokes(joke_id)
+ );
