89ddae3c516204d4413a757dbed3f4f0b3948816
odoyevsky <odoyevsky@aurora>
2023-02-10 16:54:56 +0300

    Service: create emoji store class

---
+++ joke-bot/src/main/java/com/odoyevsky/service/emojis/Emojis.java
@@ -0,0 +1,10 @@
+package com.odoyevsky.service.emojis;
+
+public class Emojis {
+    public static final String LIKE = "\uD83D\uDC4D\uD83C\uDFFB";
+    public static final String DISLIKE = "\uD83D\uDC4E\uD83C\uDFFB";
+    public static final String HEART = "❤️";
+    public static final String BROKEN_HEART = "\uD83D\uDC94";
+
+    private Emojis() {}
+}
