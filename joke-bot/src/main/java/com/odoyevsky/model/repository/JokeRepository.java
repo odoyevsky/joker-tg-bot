703587c54ab812b5d679698dcc8ef576ef475443
odoyevsky <odoyevsky@aurora>
2023-02-10 14:26:23 +0300

    Model: edit getRandomJoke() query

--- joke-bot/src/main/java/com/odoyevsky/model/repository/JokeRepository.java
+++ joke-bot/src/main/java/com/odoyevsky/model/repository/JokeRepository.java
@@ -8,9 +8,13 @@ import java.util.Optional;
 
 
 public interface JokeRepository extends CrudRepository<Joke, Long> {
-    @Query(
-            value = "SELECT j.text FROM JOKES j ORDER BY RANDOM() LIMIT 1",
-            nativeQuery = true
+    @Query(nativeQuery = true,
+            value =
+                    "SELECT j.*, max_id FROM jokes j " +
+                    "JOIN (SELECT RANDOM() * (SELECT MAX(joke_id) FROM JOKES) as max_id) as m " +
+                    "ON j.joke_id >= m.max_id " +
+                    "ORDER BY j.joke_id ASC " +
+                    "LIMIT 1"
     )
-    Optional<String> getRandomJoke();
+    Optional<Joke> getRandomJoke();
 }
