a7a6b890dda9f63e1796242a4e6cb1f2ba16dc23
odoyevsky <odoyevsky@aurora>
2023-02-13 17:34:39 +0300

    Model: add query to get a random joke of a concrete category

--- joke-bot/src/main/java/com/odoyevsky/model/repository/JokeRepository.java
+++ joke-bot/src/main/java/com/odoyevsky/model/repository/JokeRepository.java
@@ -3,6 +3,7 @@ package com.odoyevsky.model.repository;
 import com.odoyevsky.model.entity.Joke;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -11,10 +12,22 @@ public interface JokeRepository extends CrudRepository<Joke, Long> {
     @Query(nativeQuery = true,
             value =
                     "SELECT j.*, max_id FROM jokes j " +
-                    "JOIN (SELECT RANDOM() * (SELECT MAX(joke_id) FROM JOKES) as max_id) as m " +
-                    "ON j.joke_id >= m.max_id " +
-                    "ORDER BY j.joke_id ASC " +
-                    "LIMIT 1"
+                            "JOIN (SELECT RANDOM() * (SELECT MAX(joke_id) FROM JOKES) as max_id) as m " +
+                            "ON j.joke_id >= m.max_id " +
+                            "ORDER BY j.joke_id ASC " +
+                            "LIMIT 1"
     )
     Optional<Joke> getRandomJoke();
+
+    @Query(nativeQuery = true,
+            value =
+                    "SELECT j.* from jokes j\n" +
+                            "JOIN categories c\n" +
+                            "ON c.category_id = j.category_id AND \n" +
+                            "\tc.name = :category\n" +
+                            "JOIN (SELECT RANDOM() * (SELECT MAX(j.joke_id) FROM jokes j) as max_id) as m\n" +
+                            "ON j.joke_id >= m.max_id \n" +
+                            "LIMIT 1"
+    )
+    Optional<Joke> getCategoryJoke(@Param("category") String category);
 }
