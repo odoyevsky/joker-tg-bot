293991629dce7dfa04cafa9b967cc3f3d1f5ba95
odoyevsky <odoyevsky@aurora>
2023-02-16 18:09:30 +0300

    добавил эксепшены

---
+++ joke-bot/src/main/java/com/odoyevsky/exception/CategoryNotFound.java
@@ -0,0 +1,7 @@
+package com.odoyevsky.exception;
+
+public class CategoryNotFound extends Exception{
+    public CategoryNotFound(String message){
+        super(message);
+    }
+}
---
+++ joke-bot/src/main/java/com/odoyevsky/exception/JokeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.odoyevsky.exception;
+
+public class JokeNotFoundException extends Exception{
+    public JokeNotFoundException(String message){
+        super(message);
+    }
+}
