ac804f8f6b083b5faad11dfe5da6ccae5614cee3
odoyevsky <odoyevsky@aurora>
2023-02-17 13:52:26 +0300

    refactor and modify LongPollingJokeBot

---
+++ joke-bot/src/main/java/com/odoyevsky/LongPollingJokeBot.java
@@ -0,0 +1,76 @@
+package com.odoyevsky;
+
+import com.odoyevsky.config.BotConfiguration;
+import com.odoyevsky.handler.RequestHandler;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@AllArgsConstructor
+public class LongPollingJokeBot extends TelegramLongPollingBot {
+    private BotConfiguration botConfiguration;
+    private RequestHandler requestHandler;
+
+    @PostConstruct
+    private void init() {
+        addCommands(getCommands());
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        executeMessage(requestHandler.handle(update));
+    }
+
+    private void executeMessage(List<BotApiMethod<?>> messages) {
+        messages.forEach(this::executeMessage);
+    }
+
+    private void executeMessage(BotApiMethod<?> message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void addCommands(List<BotCommand> botCommands) {
+        try {
+            execute(new SetMyCommands(botCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<BotCommand> getCommands() {
+        return new ArrayList<>(
+                Arrays.asList(
+                        new BotCommand("/joke", "случайная шутка "),
+                        new BotCommand("/categories", "список тем"),
+                        new BotCommand("/favourites", "избранные шутки"),
+                        new BotCommand("/help", "справочная информация")
+                )
+        );
+    }
+
+    @Override
+    public String getBotToken() {
+        return botConfiguration.getToken();
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botConfiguration.getName();
+    }
+}
