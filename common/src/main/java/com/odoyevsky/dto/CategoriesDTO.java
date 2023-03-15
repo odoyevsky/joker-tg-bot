920a3aaea6f1c43bb16352b9a14ebfe79f5f864c
odoyevsky <odoyevsky@aurora>
2023-03-15 10:21:41 +0300

    change DTOs

--- common/src/main/java/com/odoyevsky/dto/CategoriesDTO.java
+++ common/src/main/java/com/odoyevsky/dto/CategoriesDTO.java
@@ -1,9 +1,11 @@
 package com.odoyevsky.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.Set;
 
+@AllArgsConstructor
 @Data
 public class CategoriesDTO {
     private Set<String> name;
